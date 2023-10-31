2190f14ca69bd580912ebac352681ddce207c167
hngo6 <hoang.loc.ngo@intel.com>
2023-10-31 14:42:25 +0700

    updated instruction

--- js/mfgApp.js
+++ js/mfgApp.js
@@ -6,3 +6,9 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err))
     })
   }
+
+
+  //https://svgconverter.com/pdf-to-svg
+  // step 1: split the full calendar into 4 qtr
+  // step 2: save the XLSX to PDF format
+  // step 3: convert the PDF version to SVG using above url
