8fccfdea7962480f095d92dc1ee3a1c3760bf33d
Jaden Ngo <hoang.loc.ngo@intel.com>
2024-12-09 14:54:46 +0700

    fix: update service worker file path and remove deprecated code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,56 +1,56 @@
-// On install - the application shell cached
-const cacheName = 'mfg-calendar-v1';
-const appAssets = [
-  'index.html',
-  'css/home.css',
-  'css/bootstrap.min.css',
+// // On install - the application shell cached
+// const cacheName = 'mfg-calendar-v1';
+// const appAssets = [
+//   'index.html',
+//   'css/home.css',
+//   'css/bootstrap.min.css',
 
-  'js/jquery.min.js',
-  'js/bootstrap.min.js',
-  'js/jquery-3.2.1.slim.min.js',
-  'js/mfgApp.js',
-];
+//   'js/jquery.min.js',
+//   'js/bootstrap.min.js',
+//   'js/jquery-3.2.1.slim.min.js',
+//   'js/mfgApp.js',
+// ];
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(appAssets);
-    // enable for debug
-    // console.log(appAssets);
-  })());
-});
+// self.addEventListener('install', (e) => {
+//   console.log('[Service Worker] Install');
+//   e.waitUntil((async () => {
+//     const cache = await caches.open(cacheName);
+//     console.log('[Service Worker] Caching all: app shell and content');
+//     await cache.addAll(appAssets);
+//     // enable for debug
+//     // console.log(appAssets);
+//   })());
+// });
 
-// Activate happens after install, either when the app is used for the
-// first time, or when a new version of the SW was installed.
-// We use the activate event to delete old caches and avoid running out of space.
-self.addEventListener("activate", event => {
-  event.waitUntil((async () => {
-    const names = await caches.keys();
-    await Promise.all(names.map(name => {
-      if (name !== cacheName) {
-        return caches.delete(name);
-      }
-    }));
-    await clients.claim();
-  })());
-});
+// // Activate happens after install, either when the app is used for the
+// // first time, or when a new version of the SW was installed.
+// // We use the activate event to delete old caches and avoid running out of space.
+// self.addEventListener("activate", event => {
+//   event.waitUntil((async () => {
+//     const names = await caches.keys();
+//     await Promise.all(names.map(name => {
+//       if (name !== cacheName) {
+//         return caches.delete(name);
+//       }
+//     }));
+//     await clients.claim();
+//   })());
+// });
 
 
-// This variable will save the event for later use.
-let deferredPrompt;
-self.addEventListener('beforeinstallprompt', (e) => {
-  // Prevents the default mini-infobar or install dialog from appearing on mobile
-  e.preventDefault();
-  // Save the event because you'll need to trigger it later.
-  deferredPrompt = e;
-  // Show your customized install prompt for your PWA
-  // Your own UI doesn't have to be a single element, you
-  // can have buttons in different locations, or wait to prompt
-  // as part of a critical journey.
-  showInAppInstallPromotion();
-});
+// // This variable will save the event for later use.
+// let deferredPrompt;
+// self.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevents the default mini-infobar or install dialog from appearing on mobile
+//   e.preventDefault();
+//   // Save the event because you'll need to trigger it later.
+//   deferredPrompt = e;
+//   // Show your customized install prompt for your PWA
+//   // Your own UI doesn't have to be a single element, you
+//   // can have buttons in different locations, or wait to prompt
+//   // as part of a critical journey.
+//   showInAppInstallPromotion();
+// });
 
 // self.addEventListener('push', event => {
 //   const data = event.data ? event.data.json() : {}; // Parse the incoming data
